3c4c6e12a754ed11be27dd8611b97f06aa146918
tobias1022 <tobiasvalenzuela63@gmail.com>
2024-08-19 20:33:32 -0300

    terminado

--- src/components/FormClient.vue
+++ src/components/FormClient.vue
@@ -6,9 +6,32 @@
                 <label class="form-label">Nombre</label>
                 <input v-model="nombre" type="text" class="form-control">
             </div>
-    <button @click="mostrar" class="btn btn-success">Aceptar</button>
-    <h4>{{nombre}}</h4>
-     </div>
+            <button @click="mostrar" class="btn btn-success">Aceptar</button>
+            <h4>{{nombre}}</h4>
+        </div>
+        <div class="col-lg-8">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Api</th>
+                        <th>Nombre</th>
+                        <th>Usuario</th>
+                        <th>Correo Electronico</th>
+                        <th>Sitio web</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(u,index) of lista_usuario" v-bind:key="index">
+                        <td>{{index+1}}</td>
+                        <td>{{u.name}}</td>
+                        <td>{{u.username}}</td>
+                        <td>{{u.email}}</td>
+                        <td>{{u.website}}</td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>  
  </div>
 </template>
 <script>
@@ -18,15 +41,26 @@ export default {
     name:'FormClient',
     setup() {
         let nombre = ref('Pepe')
+        let lista_usuario = ref ([])
 
         function mostrar(){
            alert(nombre.value)
         }
 
+        async function obtenerusuarios(){
+            const usuarios = await fetch('https://jsonplaceholder.typicode.com/users')
+            lista_usuario.value = await usuarios.json()
+        }
+
         return{
             nombre,
-            mostrar
+            mostrar,
+            lista_usuario,
+            obtenerusuarios
         }
     },
+    created(){
+        this.obtenerusuarios()
+    }
 }
 </script>
